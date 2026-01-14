e01f39699f179ea644295faecfceb97004df7916
ne5ermind <stepankutaev@yandex.ru>
2026-01-14 17:02:21 +0300

    updated configs

--- home.nix
+++ home.nix
@@ -135,23 +135,55 @@
       enable = true;
       enableZshIntegration = true;
       settings = {
-        format = "[$directory]($style)$git_branch$git_status\n$character";
-        add_newline = true;
+        format = ''
+          [$directory]($style)
+          $character'';
+
+        right_format = "$git_branch$git_metrics$git_status$cmd_duration";
 
         directory = {
           style = "bold blue";
-          truncate_to_repo = true;
+          truncation_length = 3;
+          truncation_symbol = "…/";
+          repo_root_style = "bold bright-blue";
+          format = "[$path]($style)[$read_only]($read_only_style) ";
         };
 
         character = {
           success_symbol = "[❯](bold magenta)";
           error_symbol = "[❯](bold red)";
+          vimcmd_symbol = "[❮](bold green)";
         };
 
         git_branch = {
-          format = "on [$symbol$branch]($style) ";
-          symbol = " ";
-          style = "bold white";
+          symbol = "△ ";
+          style = "italic bright-blue";
+          format = "[$symbol$branch]($style) ";
+        };
+
+        git_metrics = {
+          disabled = false;
+          added_style = "bold green";
+          deleted_style = "bold red";
+          format = "([+$added]($added_style) )([-$deleted]($deleted_style) )";
+        };
+
+        git_status = {
+          style = "bold red";
+          format = "[[ $all_status$ahead_behind ]]($style)";
+        };
+
+        cmd_duration = {
+          min_time = 500;
+          format = " [◄ $duration](italic white)";
+        };
+
+        add_newline = true;
+
+        nix_shell = {
+          symbol = "❄️ ";
+          format = "via [$symbol$state]($style) ";
+          style = "bold blue";
         };
       };
     };
@@ -168,7 +200,15 @@
         ll = "eza -lh --icons --grid";
         cat = "bat";
         btw = "echo 'i use nixos btw'";
-        update = "sudo nixos-rebuild switch --flake /etc/nixos#nevernix";
+        update = ''
+          (
+            cd /etc/nixos
+            sudo systemctl stop zapret-discord-youtube.service
+            sudo git add *
+            sudo git commit -m "automatic commit on config update"
+            sudo nixos-rebuild switch --flake .#nevernix
+          )
+        '';
         cfg = "cd /etc/nixos";
         matrix = "neo --colormode=0 -a -f 120 -S 7 -D ";
         ff = "fastfetch";
