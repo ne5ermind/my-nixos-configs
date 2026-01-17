80cbf841ce330491b47882d4b994bac3e2654c2a
ne5ermind <stepankutaev@yandex.ru>
2026-01-18 00:08:35 +0300

    automatic commit on config update

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelModules = [ "xt_TPROXY" ];
   time.timeZone = "Europe/Moscow";
 
   hardware.graphics = {
