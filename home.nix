5e614ddb5c681e01970df831c59c3d1d39a2574e
ne5ermind <stepankutaev@yandex.ru>
2026-01-18 13:02:36 +0300

    automatic commit on config update

--- home.nix
+++ home.nix
@@ -299,7 +299,7 @@
         tab_title_template      "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{title[:30]}{title[30:] and '…'} [{index}]"
         active_tab_title_template "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{title[:30]}{title[30:] and '…'} [{index}]"
 
-        background #000000
+        background #222222
         foreground #ffffff
         cursor #ffffff
         selection_background #ffffff
