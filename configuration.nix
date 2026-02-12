155e7da910029aeda2b5066b70fb035777e74956
ne5ermind <stepankutaev@yandex.ru>
2026-02-12 16:19:01 +0300

    automatic commit on config update

--- configuration.nix
+++ configuration.nix
@@ -17,25 +17,25 @@
     kernelModules = [ "xt_TPROXY" ];
 
     loader = {
-      efi.canTouchEfiVariables = false;
-      # systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      systemd-boot.enable = true;
 
-      grub = {
-        enable = true;
-        device = "nodev";
-        efiSupport = true;
-        useOSProber = true;
-        efiInstallAsRemovable = true;
-        theme = null;
+      #     grub = {
+      #       enable = true;
+      #       device = "nodev";
+      #       efiSupport = true;
+      #       useOSProber = true;
+      #       efiInstallAsRemovable = true;
+      #       theme = null;
 
-        extraEntries = ''
-          menuentry "Bazzite (via Chainload)" --class bazzite --class fedora {
-          insmod part_gpt
-          insmod fat
-          search --no-floppy --fs-uuid --set=root --file /EFI/fedora/shimx64.efi
-          chainloader /EFI/fedora/shimx64.efi
-        '';
-      };
+      #       extraEntries = ''
+      #         menuentry "Bazzite (via Chainload)" --class bazzite --class fedora {
+      #         insmod part_gpt
+      #         insmod fat
+      #         search --no-floppy --fs-uuid --set=root --file /EFI/fedora/shimx64.efi
+      #         chainloader /EFI/fedora/shimx64.efi
+      #       '';
+      #     };
     };
   };
 
