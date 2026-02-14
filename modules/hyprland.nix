27f089f6164467def5c2a1e7b8e07c13f6e34f10
ne5ermind <stepankutaev@yandex.ru>
2026-02-14 13:02:08 +0300

    automatic commit on config update

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -210,6 +210,11 @@
 
     extraConfig = ''
       bar_height = 20
+
+      hyprbars-button = rgb(ff4040), 10, 󰖭, hyprctl dispatch killactive
+      hyprbars-button = rgb(eeee11), 10, , hyprctl dispatch fullscreen 1
+
+      on_double_click = hyprctl dispatch fullscreen 1
     '';
   };
 }
