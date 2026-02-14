f3d25d262e9f187e19a655ebf0dd47f5a489552c
ne5ermind <stepankutaev@yandex.ru>
2026-02-14 22:07:31 +0300

    automatic commit on config update

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -147,31 +147,57 @@
         "$mainMod SHIFT, Slash, exec, dms ipc call keybinds toggle hyprland"
         "$mainMod SHIFT, P, dpms, toggle"
         "$mainMod SHIFT, C, exec, hyprctl dispatch togglefloating && hyprctl dispatch centerwindow"
-      ]
-      ++ (map (n: "$mainMod, ${toString n}, workspace, ${toString n}") [
-        1
-        2
-        3
-        4
-        5
-        6
-        7
-        8
-        9
-        0
-      ])
-      ++ (map (n: "$mainMod SHIFT, ${toString n}, movetoworkspace, ${toString n}") [
-        1
-        2
-        3
-        4
-        5
-        6
-        7
-        8
-        9
-        0
-      ]);
+
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
+
+        "$mod SHIFT, 1, movetoworkspacesilent, 1"
+        "$mod SHIFT, 2, movetoworkspacesilent, 2"
+        "$mod SHIFT, 3, movetoworkspacesilent, 3"
+        "$mod SHIFT, 4, movetoworkspacesilent, 4"
+        "$mod SHIFT, 5, movetoworkspacesilent, 5"
+        "$mod SHIFT, 6, movetoworkspacesilent, 6"
+        "$mod SHIFT, 7, movetoworkspacesilent, 7"
+        "$mod SHIFT, 8, movetoworkspacesilent, 8"
+        "$mod SHIFT, 9, movetoworkspacesilent, 9"
+        "$mod SHIFT, 0, movetoworkspacesilent, 10"
+        "$mod CTRL, c, movetoworkspace, empty"
+
+        "$mod SHIFT, left, movewindow, l"
+        "$mod SHIFT, right, movewindow, r"
+        "$mod SHIFT, up, movewindow, u"
+        "$mod SHIFT, down, movewindow, d"
+        "$mod SHIFT, h, movewindow, l"
+        "$mod SHIFT, j, movewindow, d"
+        "$mod SHIFT, k, movewindow, u"
+        "$mod SHIFT, l, movewindow, r"
+
+        "$mod CTRL, left, resizeactive, -80 0"
+        "$mod CTRL, right, resizeactive, 80 0"
+        "$mod CTRL, up, resizeactive, 0 -80"
+        "$mod CTRL, down, resizeactive, 0 80"
+        "$mod CTRL, h, resizeactive, -80 0"
+        "$mod CTRL, j, resizeactive, 0 80"
+        "$mod CTRL, k, resizeactive, 0 -80"
+        "$mod CTRL, l, resizeactive, 80 0"
+
+        "$mod ALT, left, moveactive,  -80 0"
+        "$mod ALT, right, moveactive, 80 0"
+        "$mod ALT, up, moveactive, 0 -80"
+        "$mod ALT, down, moveactive, 0 80"
+        "$mod ALT, h, moveactive,  -80 0"
+        "$mod ALT, j, moveactive, 0 80"
+        "$mod ALT, k, moveactive, 0 -80"
+        "$mod ALT, l, moveactive, 80 0"
+      ];
 
       bindm = [
         "$mainMod, mouse:272, movewindow"
