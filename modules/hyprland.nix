855617ff18c65e7d84152e67373942c5b99d566e
ne5ermind <stepankutaev@yandex.ru>
2026-02-14 22:41:04 +0300

    automatic commit on config update

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -115,11 +115,11 @@
       };
 
       "plugin:hyprbars" = {
-        bar_height = 20;
+        bar_height = 30;
         bar_color = "rgba(1a1a1aaa)";
         hyprbars-button = [
-          "rgb(ff4040), 12,   , hyprctl dispatch killactive"
-          "rgb(eeee11), 12, , hyprctl dispatch fullscreen 1"
+          "hyprbars-button = rgb(ff4040), 10, 󰖭, hyprctl dispatch killactive"
+          "hyprbars-button = rgb(eeee11), 10, , hyprctl dispatch fullscreen 1"
         ];
       };
 
