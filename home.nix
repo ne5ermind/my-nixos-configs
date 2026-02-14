89c9f969145b679dab374b0d70a1b6e173ecf4a6
ne5ermind <stepankutaev@yandex.ru>
2026-02-14 13:00:30 +0300

    automatic commit on config update

--- home.nix
+++ home.nix
@@ -163,7 +163,7 @@
           #         truncation_length = 3;
           #         truncation_symbol = "…/";
           repo_root_style = "bold bright-blue";
-          format = "[$path]($style)[$read_only]($read_only_style) ";
+          format = "[$path]($style)[$read_only] ";
         };
 
         character = {
