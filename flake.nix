6ee50aebaac298ae4fdae2061a0d079a66a269cc
ne5ermind <stepankutaev@yandex.ru>
2026-03-16 20:52:20 +0300

    automatic commit on config update

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     #nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    flake-utils.url = "github:numtide/flake-utils";
+    # flake-utils.url = "github:numtide/flake-utils";
 
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
@@ -59,41 +59,41 @@
     }@inputs:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
+      #     pkgs = import nixpkgs {
+      #       inherit system;
+      #       config.allowUnfree = true;
+      #     };
 
-      happ-proxy = pkgs.stdenv.mkDerivation rec {
-        pname = "happ";
-        version = "2.5.2";
-        src = pkgs.fetchurl {
-          url = "https://github.com/Happ-proxy/happ-desktop/releases/download/2.5.2/Happ.linux.x64.deb";
-          hash = "sha256-fxCgsM6byhKtFw206yEvStAPRR8JhhiOw8OQI5U1/9c=";
-        };
-        nativeBuildInputs = with pkgs; [
-          dpkg
-          autoPatchelfHook
-          # makeWrapper
-        ];
-        buildInputs = with pkgs; [
-          glibc
-          gcc-unwrapped
-        ];
-        unpackPhase = "dpkg-deb -x $src .";
-        installPhase = ''
-          mkdir -p $out
-          cp -r usr/* $out/
+      #     happ-proxy = pkgs.stdenv.mkDerivation rec {
+      #       pname = "happ";
+      #       version = "2.5.2";
+      #       src = pkgs.fetchurl {
+      #         url = "https://github.com/Happ-proxy/happ-desktop/releases/download/2.5.2/Happ.linux.x64.deb";
+      #         hash = "sha256-fxCgsM6byhKtFw206yEvStAPRR8JhhiOw8OQI5U1/9c=";
+      #       };
+      #       nativeBuildInputs = with pkgs; [
+      #         dpkg
+      #         autoPatchelfHook
+      #         # makeWrapper
+      #       ];
+      #       buildInputs = with pkgs; [
+      #         glibc
+      #         gcc-unwrapped
+      #       ];
+      #       unpackPhase = "dpkg-deb -x $src .";
+      #       installPhase = ''
+      #         mkdir -p $out
+      #         cp -r usr/* $out/
 
-          substituteInPlace $out/share/applications/*.desktop \
-            --replace "Exec=/usr/bin/your-app-binary" "Exec=$out/bin/your-app-binary"
-        '';
-      };
+      #         substituteInPlace $out/share/applications/*.desktop \
+      #           --replace "Exec=/usr/bin/your-app-binary" "Exec=$out/bin/your-app-binary"
+      #       '';
+      #     };
     in
     {
       nixosConfigurations.nevernix = nixpkgs.lib.nixosSystem {
         inherit system;
-        specialArgs = { inherit inputs happ-proxy; };
+        specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
@@ -103,9 +103,9 @@
               useUserPackages = true;
               users.never = import ./home.nix;
               backupFileExtension = "hm-bak";
-              extraSpecialArgs = {
-                inherit inputs happ-proxy;
-              };
+              #             extraSpecialArgs = {
+              #               inherit inputs happ-proxy;
+              #             };
             };
           }
         ];
