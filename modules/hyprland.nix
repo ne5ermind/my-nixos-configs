4b6eda0dd2f85fb6747f3c76a0d0599408559f05
ne5ermind <stepankutaev@yandex.ru>
2026-03-23 17:04:02 +0300

    automatic commit on config update

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -99,7 +99,7 @@
         kb_layout = "us,ru";
         kb_options = "grp:alt_shift_toggle,caps:escape";
         follow_mouse = 1;
-        accel_profile = "flat";
+        # accel_profile = "flat";
         touchpad = {
           natural_scroll = false;
           disable_while_typing = true;
@@ -113,7 +113,7 @@
         }
         {
           name = "ftcs0038:00-2808:0106-touchpad";
-          sensitivity = 1;
+          sensitivity = 0.3;
         }
       ];
 
