91cc5939ecac69498c052795bb9eda0c445148e6
ne5ermind <stepankutaev@yandex.ru>
2026-04-11 19:56:16 +0300

    automatic commit on config update

--- flakes/flclashx.nix
+++ flakes/flclashx.nix
@@ -4,8 +4,8 @@ pkgs.appimageTools.wrapType2 {
   pname = "flclashx";
   version = "0.3.2";
   src = pkgs.fetchurl {
-    url = "https://github.com/pluralplay/FlClashX";
-    sha256 = "sha256-iaGKJ8RUyG2HkwzZQMcjWVqyvSGqXQlKTwcuDBa5XsM=";
+    url = "https://github.com/pluralplay/FlClashX/releases/download/v0.3.2/FlClashX-linux-amd64.AppImage";
+    sha256 = "sha256:45b2f533a58ab14a31b7cde40e6923a16b36be8201ce9f4fe216d29f9303068c";
   };
   extraPkgs =
     pkgs: with pkgs; [
