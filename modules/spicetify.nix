154ac924d24cbb2928ea5da53b19bf7615242d59
ne5ermind <stepankutaev@yandex.ru>
2026-04-11 19:44:59 +0300

    automatic commit on config update

--- modules/spicetify.nix
+++ modules/spicetify.nix
@@ -2,7 +2,7 @@
 {
   programs.spicetify = {
     enable = true;
-    theme = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system}.themes.blackout;
+    theme = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system}.themes.nightlight;
     enabledExtensions =
       with inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system}.extensions; [
         adblock
